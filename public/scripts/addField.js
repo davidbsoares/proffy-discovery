d27edb753de61139d78d2e77fa0e02b420145d25
David Brum Soares <david.bsoares@hotmail.com>
2020-12-21 15:15:08 -0300

    projeto finalizado

---
+++ public/scripts/addField.js
@@ -0,0 +1,24 @@
+// Procurar o botão
+document.querySelector("#add-time")
+
+// Quando clicar no botão
+.addEventListener('click', cloneField)
+
+// Executar uma ação
+function cloneField(){   
+    // Duplicar os campos. Que campos?
+    const newFieldContainer = document.querySelector(".schedule-item").cloneNode(true) //boolean = true ou false
+
+    // pegar os campos. Que campos?
+    const fields = newFieldContainer.querySelectorAll('input')
+
+    // para cada campo, limpar
+    fields.forEach(function(field){
+        // pegar o field do momento e limpá-lo
+        field.value = ""
+    })
+
+    // Colocar na página. Onde?
+    document.querySelector("#schedule-items").appendChild(newFieldContainer);
+
+}
